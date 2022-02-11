7217bd10d6fe995c1d17473cb91ea238b941aa3c
sway2020 <65274619+sway2020@users.noreply.github.com>
2022-02-11 15:42:43 -0800

    Update to Cities Harmony API 2.1.0

--- src/ModInfo.cs
+++ src/ModInfo.cs
@@ -10,7 +10,7 @@ namespace YetAnotherToolbar
 {
     public class ModInfo : IUserMod
     {
-        public const string version = "1.1.5";
+        public const string version = "1.1.5-beta2";
         public const bool isBeta = true;
 
         public string Name
